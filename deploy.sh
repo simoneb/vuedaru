56be38c4b959dcceaa349eed745e1ea08f78b126
Simone Busoli <simone.busoli@gmail.com>
2018-08-22 02:03:33 +0200

    github deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 npm run build
-cd dist/
 git init
 git add -A
 git commit -m"deploy"
-git push -f https://simoneb@bitbucket.org/simoneb/simoneb.bitbucket.io.git master
+git push https://github.com/simoneb/vuedaru.git master
 cd --
